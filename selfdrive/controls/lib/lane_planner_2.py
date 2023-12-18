96cd8579ecec39f66994dbc4e3040e16d19778ca
ajouatom <ajouatom@gmail.com>
2023-12-18 19:15:02 +0900

    fix...

--- selfdrive/controls/lib/lane_planner_2.py
+++ selfdrive/controls/lib/lane_planner_2.py
@@ -369,7 +369,7 @@ class LanePlanner:
       self.lane_offset_filtered.update(offset_curve+offset_lane)
       lane_path_y = path_from_right_lane if r_prob > 0.5 or r_prob > l_prob else path_from_left_lane
       lane_path_y += self.lane_offset_filtered.x
-      self.debugText = "vC={:.2f},offset={:.1f},LP={:.1f},RP={:.1f},LW={:.1f},RW={:.1f}".format(curvature, self.lane_offset_filtered.x, l_prob, r_prob, self.lane_width_left_filtered.x, self.lane_width_right_filtered.x)
+      self.debugText = "vC={:.2f},offset={:.2f},LP={:.1f},RP={:.1f},LW={:.1f},RW={:.1f}".format(curvature, self.lane_offset_filtered.x, l_prob, r_prob, self.lane_width_left_filtered.x, self.lane_width_right_filtered.x)
 
       #lane_path_y = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
       safe_idxs = np.isfinite(self.ll_t)
