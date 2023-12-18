46c020be10aa1c567937060b58f2acd64b507680
ajouatom <ajouatom@gmail.com>
2023-12-18 21:31:38 +0900

    ff

--- selfdrive/controls/lib/lane_planner_2.py
+++ selfdrive/controls/lib/lane_planner_2.py
@@ -368,7 +368,6 @@ class LanePlanner:
           offset_lane = -self.adjustLaneOffset
       self.lane_offset_filtered.update(offset_curve+offset_lane)
       lane_path_y = path_from_right_lane if r_prob > 0.5 or r_prob > l_prob else path_from_left_lane
-      lane_path_y += self.lane_offset_filtered.x
       self.debugText = "vC={:.2f},offset={:.2f},LP={:.1f},RP={:.1f},LW={:.1f},RW={:.1f}".format(curvature, self.lane_offset_filtered.x, l_prob, r_prob, self.lane_width_left_filtered.x, self.lane_width_right_filtered.x)
 
       #lane_path_y = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
@@ -392,6 +391,6 @@ class LanePlanner:
       #sLogger.Send("vC--- LX" + "{:.1f}".format(self.lll_y[0]) + " RX" + "{:.1f}".format(self.rll_y[0]) + " LW" + "{:.1f}".format(self.lane_width) + " LP" + "{:.1f}".format(l_prob) + " RP" + "{:.1f}".format(r_prob) + " RS" + "{:.1f}".format(self.rll_std) + " LS" + "{:.1f}".format(self.lll_std))
       #self.debugText = "vC--- LX" + "{:.1f}".format(self.lll_y[0]) + " RX" + "{:.1f}".format(self.rll_y[0]) + " LW" + "{:.1f}".format(self.lane_width) + " LP" + "{:.1f}".format(l_prob) + " RP" + "{:.1f}".format(r_prob) + " RS" + "{:.1f}".format(self.rll_std) + " LS" + "{:.1f}".format(self.lll_std)
 
-    path_xyz[:, 1] += CAMERA_OFFSET
+    path_xyz[:, 1] += (CAMERA_OFFSET + self.lane_offset_filtered.x)
 
     return path_xyz
