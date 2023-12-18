10b1058cd6a45c930fbd682e9b423a899afb0f18
ajouatom <ajouatom@gmail.com>
2023-12-18 09:20:41 +0900

    fix..

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -1028,14 +1028,6 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .1, alert_rate=0.75),
   },
 
-  # Random Events
-  RandomEventName.firefoxSteerSaturated: {
-    ET.WARNING: Alert(
-      "Turn Exceeds Steering Limit",
-      "IE Has Stopped Responding...",
-      AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.firefox, 2.),
-  },
   EventName.laneChangeRoadEdge: {
     ET.WARNING: Alert(
       "Car Detected in Blindspot or RoadEdge",
