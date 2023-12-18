bc70e00187bd3d8fa87ddfc8d6cf935ae86127bf
ajouatom <ajouatom@gmail.com>
2023-12-18 22:18:53 +0900

    ff

--- selfdrive/controls/lib/lane_planner_2.py
+++ selfdrive/controls/lib/lane_planner_2.py
@@ -366,12 +366,12 @@ class LanePlanner:
           offset_lane = self.adjustLaneOffset
         elif self.lane_width_right_filtered.x > 2.5:
           offset_lane = -self.adjustLaneOffset
-      self.lane_offset_filtered.update(clip(offset_curve + offset_lane, - self.adjustLaneOffset, self.adjustLaneOffset))
 
       #lane_path_y = path_from_right_lane if r_prob > 0.5 or r_prob > l_prob else path_from_left_lane
       lane_path_y = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
-
-      self.debugText = "d={:.2f},vC={:.2f},offset={:.2f},LP={:.1f},RP={:.1f},LW={:.1f},RW={:.1f}".format(lane_path_y[0] - path_xyz[:,1][0], self.d_prob, curvature, self.lane_offset_filtered.x, l_prob, r_prob, self.lane_width_left_filtered.x, self.lane_width_right_filtered.x)
+      diff_center = lane_path_y[0] - path_xyz[:,1][0] 
+      self.lane_offset_filtered.update(clip(offset_curve + offset_lane + diff_center, - self.adjustLaneOffset, self.adjustLaneOffset))
+      self.debugText = "d={:.2f},vC={:.2f},offset={:.2f},LP={:.1f},RP={:.1f},LW={:.1f},RW={:.1f}".format(diff_center, self.d_prob, curvature, self.lane_offset_filtered.x, l_prob, r_prob, self.lane_width_left_filtered.x, self.lane_width_right_filtered.x)
       if False:
         safe_idxs = np.isfinite(self.ll_t)
         if safe_idxs[0]:
