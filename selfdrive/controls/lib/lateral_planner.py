507ed8e4a4cf0be1dab8fac985fc20831868e424
ajouatom <ajouatom@gmail.com>
2023-12-20 09:23:07 +0900

    fix..

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -214,13 +214,13 @@ class LateralPlanner:
       self.LP.lane_change_multiplier = 1.0
 
     # lanelines calculation?
-    self.lanefull_mode = self.useLaneLineMode
+    self.LP.lanefull_mode = self.useLaneLineMode
     self.LP.lane_width_left = self.DH.lane_width_left
     self.LP.lane_width_right = self.DH.lane_width_right
     self.LP.curvature = measured_curvature
     self.path_xyz = self.LP.get_d_path(sm['carState'], self.v_ego, self.t_idxs, self.path_xyz, self.vcurv)
     self.latDebugText = self.LP.debugText
-    self.lanelines_active = True if self.LP.d_prob > 0.3 else False
+    self.lanelines_active = True if self.LP.d_prob > 0.3 and self.LP.lanefull_mode else False
 
     self.path_xyz[:, 1] += self.pathOffset
 
