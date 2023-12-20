2bddd39f1e1e0b9220b7ad75af25d41ea920faa2
ajouatom <ajouatom@gmail.com>
2023-12-20 12:01:04 +0900

    1sec

--- selfdrive/controls/lib/lane_planner_2.py
+++ selfdrive/controls/lib/lane_planner_2.py
@@ -373,7 +373,7 @@ class LanePlanner:
     #lane_path_y = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
 
     # offset_center = lane_line_center - laneless_center
-    x_center_dist = v_ego * 3.0  # after 1sec x dist
+    x_center_dist = v_ego * 1.0  # after 1sec x dist
     lane_path_y_center = interp(x_center_dist, self.ll_x, lane_path_y)
     path_xyz_y_center = interp(x_center_dist, path_xyz[:,0], path_xyz[:,1])
     #diff_center = lane_path_y[5] - path_xyz[:,1][5] if not self.lanefull_mode else 0.0
