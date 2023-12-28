c904aefebe559fe1a056496deaa060967bf3d900
ajouatom <ajouatom@gmail.com>
2023-12-28 16:31:31 +0900

    laneWidth left/right filter more

--- selfdrive/controls/lib/lane_planner_2.py
+++ selfdrive/controls/lib/lane_planner_2.py
@@ -71,8 +71,8 @@ class LanePlanner:
     self.debugText = ""
     self.lane_width_left = 0.0
     self.lane_width_right = 0.0
-    self.lane_width_left_filtered = FirstOrderFilter(1.0, 0.95, DT_MDL)
-    self.lane_width_right_filtered = FirstOrderFilter(1.0, 0.95, DT_MDL)
+    self.lane_width_left_filtered = FirstOrderFilter(1.0, 0.98, DT_MDL)
+    self.lane_width_right_filtered = FirstOrderFilter(1.0, 0.98, DT_MDL)
     self.lane_offset_filtered = FirstOrderFilter(0.0, 0.98, DT_MDL)
 
     self.lanefull_mode = False
