7f2200976266c45565a1ded8a59d95328ce26aa2
ajouatom <ajouatom@gmail.com>
2023-12-31 09:16:36 +0900

    ff

--- selfdrive/carrot/road_speed_limiter.py
+++ selfdrive/carrot/road_speed_limiter.py
@@ -444,6 +444,8 @@ def main():
         if ret or now - prev_recvTime > 2.0: # 수신값이 있거나, 2.0초가 지난경우 데이터를 초기화함.
           nTBTTurnType = nSdiType = nSdiSpeedLimit = nSdiPlusType = nSdiPlusSpeedLimit = nSdiBlockType = -1
           nSdiBlockSpeed = nRoadLimitSpeed = -1
+          roadcate = 8
+          nLaneCount = 0
 
         nSdiDist -= delta_dist
         nSdiPlusDist -= delta_dist
@@ -476,6 +478,8 @@ def main():
           nTBTDist = float(server.get_apilot_val("nTBTDist", nTBTDist))
           nRoadLimitSpeed = int(server.get_apilot_val("nRoadLimitSpeed", nRoadLimitSpeed))
           roadcate = int(server.get_apilot_val("roadcate", roadcate))
+          nLaneCount = int(server.get_apilot_val("nLaneCount", roadcate))
+          roadcate = 8 if nLaneCount == 0 else roadcate
           #print("roadcate=", roadcate)
 
         #print("O:{:.1f},{:.1f},{:.1f},{:.2f}".format(nSdiDist, nSdiPlusDist, nTBTDist, delta_dist))
